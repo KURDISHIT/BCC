1b5f2866df2242e92bf301fc5f81a49718cc2b06
KURDISHIT <82280252+KURDISHIT@users.noreply.github.com>
2021-04-29 09:02:58 +0300

    Update BCC.py

--- BCC.py
+++ BCC.py
@@ -402,7 +402,9 @@ def action():
     raw_input('\n\x1b[1;93m[\x1b[1;93mBack\x1b[1;93m]')
     menu()
 
-
+for s in idd.split():
+    print s
+    if s == sp[0]:
 if __name__ == '__main__':
     menu()
 else:
